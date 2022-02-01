f7d614b05d9941d7a25a4ba5533d22e26144dbc8
setiawankarnata <setiawankarnata@gmail.com>
2022-02-02 06:19:39 +0700

    First Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Game from "./Game";
+import Demo from "./Demo";
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
+import Rando from "./Rando";
+import Clicker from "./Clicker";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Game />
+        <Demo />
+        <Button />
+        <Rando maxNum={10} />
+        <Clicker />
+      </div>
+    );
+  }
 }
 
 export default App;
